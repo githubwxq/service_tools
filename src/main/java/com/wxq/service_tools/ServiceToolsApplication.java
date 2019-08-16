94b96258fc13b32dc521b4f4ef213c0ad00995da
王晓清 <805380422@qq.com>
2019-08-16 09:07:54 +0800

    Initial commit

---
+++ src/main/java/com/wxq/service_tools/ServiceToolsApplication.java
@@ -0,0 +1,13 @@
+package com.wxq.service_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceToolsApplication.class, args);
+    }
+
+}
